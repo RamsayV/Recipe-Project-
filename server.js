3b21e8a62a21f1858ccf2f8cb2490ca1689d82a6
Ramsay <ramsayveitch123@gmail.com>
2023-10-16 13:04:09 +0100

    netilify push for backend

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import serverless from "serverless-http";
 
 
 const app = express();
 
 app.use(cors());
-
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+export const handler = serverless(app);
 
 mongoose.connect(process.env.RECIPE_DATABASE);
 
